432edcf96ebc359613fabac9384a09c614cc2de3
Jake Brehm <jake.m.brehm@gmail.com>
2019-12-15 21:44:26 -0500

    Added comments to epicycler.js and renamed main.html to index.html.

--- epicycler.js
+++ epicycler.js
@@ -1,100 +1,132 @@
+// Initialize the state variable and its possible states
+let state = -1;
 const DRAWING = 0;
 const FOURIER = 1;
-
-let x = [];
-let fourierX;
-let time = 0;
-let path = [];
-
+// Initialize an array to hold drawing data
 let drawing = [];
-let state = -1;
+// Initialize an array for the input signal and its fourier transform
+let inputSignal = [];
+let fourierSignal;
+// Initialize the path array and the time variable
+let path = [];
+let time = 0;
 
 function setup() {
-    // Initialize the canvas
+    // Initialize the canvas and make it fill the page without scrollbars
     var canvas = createCanvas(windowWidth, windowHeight);
-    // Make it fill the page without scrollbars
     document.body.style.margin = 0;
     canvas.style('display', 'block');
 }
 
 function mousePressed() {
+    // Enter the drawing state
     state = DRAWING;
+    // Reset program variables
     drawing = [];
-    x = [];
-    time = 0;
+    inputSignal = [];
     path = [];
+    time = 0;
 }
 
 function mouseReleased() {
+
+    // Enter the fourier state
     state = FOURIER;
     
+    // Iterate through the drawing points and create an input signal
     for (let i = 0; i < drawing.length; i++) {
-
         const complex = new ComplexNumber(drawing[i].x, drawing[i].y);
-        // TODO: Rename x to signal
-        x.push(complex);
+        inputSignal.push(complex);
     }
 
-    fourierX = discreteFourierTransform(x);
-    fourierX.sort((a, b) => b.amp - a.amp);
+    // Calculate the fourier transform of the signal and sort by amplitude
+    fourierSignal = discreteFourierTransform(inputSignal);
+    fourierSignal.sort((a, b) => b.amp - a.amp);
 
 }
 
 function windowResized() {
+    // Resize the canvas to the new window width and height
     resizeCanvas(windowWidth, windowHeight);
 }
 
 function epicycles(x, y, rotation, fourier) {
+
+    // Iterate through the fourier signal to draw each epicycle and radial line
     for (let i = 0; i < fourier.length; i++) {
-        let prevx = x;
-        let prevy = y;
+
+        // Keep track of last iteration's x and y values
+        let x0 = x;
+        let y0 = y;
+        // Obtain the frequency, radius, and amplitude
         let freq = fourier[i].freq;
         let radius = fourier[i].amp;
         let phase = fourier[i].phase;
+
+        // Perform the relevant calculations and add to x and y
         x += radius * cos(freq * time + phase + rotation);
         y += radius * sin(freq * time + phase + rotation);
 
-        stroke(255, 100);
+        // Draw the epicycles
+        stroke(255, 255, 0, 0.25*255);
         noFill();
-        ellipse(prevx, prevy, radius * 2);
-        stroke(255);
-        line(prevx, prevy, x, y);
+        ellipse(x0, y0, radius * 2);
+        // Draw the radial lines
+        stroke(255, 255, 0, 0.50*255);
+        line(x0, y0, x, y);
     }
 
+    // Return a vector pointing to the final coordinate
     return createVector(x, y);
 
 }
 
 function draw() {
+
+    // Set the background color to black
     background(0);
 
+    // Behave differently depending on whether the user is drawing or not
     if (state == DRAWING) {
+
+        // Create vectors based on the location of the user's mouse
         let point = createVector(mouseX - width / 2, mouseY - height / 2);
         drawing.push(point);
+
+        // Draw the points
         stroke(255);
         noFill();
         beginShape();
-        for (let v of drawing) {
-            vertex(v.x + width / 2, v.y + height / 2);
+        for (let pair of drawing) {
+            vertex(pair.x + width / 2, pair.y + height / 2);
         }
         endShape();
+
     } else if (state == FOURIER) {
-        let v = epicycles(width / 2, height / 2, 0, fourierX);
+        
+        // Calculate the epicycles and add to the front of the path array
+        let v = epicycles(width / 2, height / 2, 0, fourierSignal);
         path.unshift(v);
     
+        // Begin drawing the shape
         beginShape();
         noFill();
         for (let i =0; i < path.length; i++) {
+            stroke(255);
             vertex(path[i].x, path[i].y);
         }
         endShape();
     
-        const dt = TWO_PI / fourierX.length;
+        // Calculate the time step and increment the time variable
+        const dt = TWO_PI / fourierSignal.length;
         time += dt;
     
+        // If the drawing is finished, reset the time and path variables
         if (time > TWO_PI) {
             time = 0;
             path = [];
         }
+
     }
+
 }
